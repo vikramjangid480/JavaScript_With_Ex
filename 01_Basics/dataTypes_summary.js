c9638845dae61060fbfa61ef181ba4f910a7cb6c
vikramjangid480 <146922165+vikramjangid480@users.noreply.github.com>
2023-10-31 06:28:56 +0000

    change in lesson

---
+++ 01_Basics/dataTypes_summary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 types : string , Number , Boolearn , undefind , Symbol , BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLogged = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol ('123')
+
+
+
+console.log(id === anthoreId);
+
+const bigNumber = 23456787543445467n 
+
+
+
+
+
+
+// Reference (Non primitive)
+
+// Array , Object , Functions
+
+const heros = ["Vikram" , "jits" , "Manish"];
+let muObj =  {
+    name: 'Vikram',
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log ("Hello world");
+
+} 
+
+console.log(typeof bigNumber);
